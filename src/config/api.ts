ea3444bbf679f5e5c869701d84bda5937663e8ec
jadsonfe <jadson.a.f.s.l@gmail.com>
2026-01-29 17:35:40 -0300

    fix: testando resolução de problema de login

--- src/config/api.ts
+++ src/config/api.ts
@@ -8,7 +8,8 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   paramsSerializer: querySerializer,
   headers: {
-    platform: "web",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    
   },
 });
 
